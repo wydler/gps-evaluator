393942c16017b4ba1853f1dfb93977ec2b90fd88
wydler <michael.wydler@googlemail.com>
2012-11-16 23:40:19 +0100

    Improve nmea parser and json encoder.

--- server/gps2maps.py
+++ server/gps2maps.py
@@ -1,40 +1,51 @@
 import serial
-import codecs
+import json
+import time
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+def dms2deg(dms):
+	dms = dms.split('.')
+	d = float(dms[0][:-2])
+	m = float(dms[0][-2:])/60
+	s = float(dms[1])*60/10000/3600
 
-char = ''
-satz = ''
+	return round(d + m + s, 6)
+
+#ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = open('gps.log', 'r+')
+print ser
+
+sentence = ''
+data = {}
 
 while 1:
-	c = ser.read()
-	if char == '\n':
-		print satz[:-1]
-		if satz.startswith('GPGGA'):
-			sp = satz.split(',')
-			#print sp
+	#c = ser.read()
+	c = ser.read(1)
+	if c == '\n':
+		s = sentence[1:-4].split(',')
+		if s[0] == 'GPGGA':
+			data["time"] = s[1][0:2]+":"+s[1][2:4]+":"+s[1][4:6]
+			data["latitude"] = [dms2deg(s[2]), s[3]]
+			data["longitude"] = [dms2deg(s[4]), s[5]]
+			data["altitude"] = [s[9], s[10]]
+			data["quality"] = s[6]
+			data["satellites"] = s[7]
+			#print data
+		elif s[0] == 'GPGSA':
+			data["fix"] = s[2]
+			data["pdop"] = s[15]
+			data["hdop"] = s[16]
+			data["vdop"] = s[17]
+		elif s[0] == 'GPRMC':
+			data["date"] = s[9][0:2]+"."+s[9][2:4]+".20"+s[9][4:6]
 
-			lat = sp[2].split('.')
-			lat_d = lat[0][:-2]
-			lat_m = lat[0][-2:]
-			lat_s = lat[1]
-			lat_g = float(lat_d) + float(lat_m)/60 + ((float(lat_s)/10000)*60)/3600
+		sentence = ''
+		print data
 
-			lng = sp[4].split('.')
-			lng_d = lng[0][:-2]
-			lng_m = lng[0][-2:]
-			lng_s = lng[1]
-			lng_g = float(lng_d) + float(lng_m)/60 + ((float(lng_s)/10000)*60)/3600
+		f = open('data.json', 'w+')
+		f.write(json.JSONEncoder().encode(data))
+		f.close()
 
-			sa = '{"latitude": ' + str(lat_g) + ', "longitude": ' + str(lng_g) + '}'
-			#print sa
-
-			f = open('data.json', 'w+')
-			f.write(sa)
-			f.close()
-
-		satz = ''
-		char = ''
+		time.sleep(1)
 	else:
-		satz += c
-		char = c
+		sentence += c
+
