5550681953aec047a36057ef1e9d5c35777fb03d
root <root@caehp06.(none)>
2012-12-21 13:45:27 +0100

    Change serial receiver.

--- server/clkdiffd.py
+++ server/clkdiffd.py
@@ -5,16 +5,17 @@ import struct
 import array
 import time
 
-#ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1, parity=serial.PARITY_EVEN)
+bytes = None
 
 while 1:
-	#cnt = ser.readinfo(bytes)
+	bytes = ser.read(8)
+#	print bytes
 	cnt = 8
-	bytes = array.array('B', '\x00\x0f\x42\x40\x00\x00\x00\x01')
-
-	if (cnt == 8):
-		cnts = struct.unpack('>II', bytes)
+	#bytes = array.array('B', '\x00\x0f\x42\x40\x00\x00\x00\x01')
+	
+	if (len(bytes) == 8):
+		cnts = struct.unpack('<II', bytes)
 		print "Counter 1: " + str(cnts[0])
 		print "Counter 2: " + str(cnts[1])
-
 	time.sleep(0.5)
